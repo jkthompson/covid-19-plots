51fe0229e150b0b8277197fd894662faba46cd32
Jeff <jeffreykeatingthompson@gmail.com>
2020-06-30 07:29:30 -0700

    mount google drive, 6-30-2020 udpate

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -p 8888:8888 -v "$PWD":/home/jovyan covid
+docker run -it -p 8888:8888 -v "$PWD":/home/jovyan -v "/mnt/chromeos/GoogleDrive/MyDrive/covid_plots":/home/jovyan/gdrive covid
